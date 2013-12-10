22cb5bc99af43e8f172a16f6d64d60bef89b07a2
Lateef Jackson <lateef.jackson@gmail.com>
2013-12-09 22:56:57 -0800

    Initial idea for helper test library

---
+++ http.go
@@ -0,0 +1,69 @@
+package mctest
+
+import (
+  "bytes"
+  "fmt"
+  "net/http"
+  "testing"
+)
+
+type MockResponse struct {
+  bytes.Buffer
+  Head       http.Header
+  StatusCode int
+}
+
+func (mr *MockResponse) Header() http.Header {
+  return mr.Head
+}
+
+func (mr *MockResponse) WriteHeader(code int) {
+  mr.StatusCode = code
+}
+
+type MockTestResponse struct {
+  MockResponse
+  T *testing.T
+}
+
+// AssertBody ... Wrapper to provide an typing savings
+func (mtr *MockTestResponse) AssertBody(b string) {
+  if mtr.String() != b {
+    mtr.T.Fatalf("Response body is %s asserted that it is %s", mtr.String(), b)
+  }
+}
+
+// AssertCode ... Helper method to validate the right status code
+func (mtr *MockTestResponse) AssertCode(c int) {
+  if mtr.StatusCode != c {
+    mtr.T.Fatalf("Response StatusCode is %d asserted that it is %d", mtr.StatusCode, c)
+  }
+}
+
+// AssertHeaders ... Helper method to validate the proper headers retuned
+func (mtr *MockTestResponse) AssertHeaders(t *testing.T, expectedHeaders map[string]string) {
+  if len(expectedHeaders) != len(mtr.Head) {
+    mtr.T.Fatalf("expected %v headers; got %v. Returned headers: %v", len(expectedHeaders), len(mtr.Head), mtr.Head)
+  }
+  for key := range expectedHeaders {
+    if val := mtr.Head.Get(key); val != expectedHeaders[key] {
+      mtr.T.Fatalf("expected header %v to be '%v'; got '%v'", key, expectedHeaders[key], val)
+    }
+  }
+}
+
+func NewMockTestResponse(t *testing.T) *MockTestResponse {
+  return &MockTestResponse{MockResponse: MockResponse{Buffer: *bytes.NewBufferString(""), Head: http.Header{}, StatusCode: 200}, T: t}
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "HomeHandler")
+}
+
+func TestHome(t *testing.T) {
+  req, _ := http.NewRequest("GET", "/path/to/handler", nil)
+  resp := NewMockTestResponse(t)
+  HomeHandler(resp, req)
+  resp.AssertBody("HomeHandler")
+  resp.AssertCode(200)
+}
