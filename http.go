0fd78d448f9c3f6d73532af0ec0cfaa3f476129d
Lateef Jackson <lateef.jackson@gmail.com>
2015-04-28 23:13:43 -0700

    Add AssertJson

--- http.go
+++ http.go
@@ -1,65 +1,86 @@
 package mctest
 
 import (
-  "bytes"
-  "net/http"
-  "testing"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"reflect"
+	"testing"
 )
 
 // MockResponse [...] Create a mock http response implementaiton
 type MockResponse struct {
-  bytes.Buffer
-  Head       http.Header
-  StatusCode int
+	bytes.Buffer
+	Head       http.Header
+	StatusCode int
 }
 
 // Header ...  Returns the header
 func (mr *MockResponse) Header() http.Header {
-  return mr.Head
+	return mr.Head
 }
 
 // WriteHeader ... Write a response code to the header
 func (mr *MockResponse) WriteHeader(code int) {
-  mr.StatusCode = code
+	mr.StatusCode = code
 }
 
 // MockTestResponse [...] Simple wrapper to save typing
 type MockTestResponse struct {
-  MockResponse
-  T *testing.T
+	MockResponse
+	T *testing.T
 }
 
 // AssertBody ... Wrapper to provide an typing savings
 func (mtr *MockTestResponse) AssertBody(b string) bool {
-  if mtr.String() != b {
-    mtr.T.Errorf("Response body is %s asserted that it is %s", mtr.String(), b)
-    return false
-  }
-  return true
+	if mtr.String() != b {
+		mtr.T.Errorf("Response body is %s asserted that it is %s", mtr.String(), b)
+		return false
+	}
+	return true
 }
 
 // AssertCode ... Helper method to validate the right status code
 func (mtr *MockTestResponse) AssertCode(c int) bool {
-  if mtr.StatusCode != c {
-    mtr.T.Errorf("Response StatusCode is %d asserted that it is %d", mtr.StatusCode, c)
-    return false
-  }
-  return true
+	if mtr.StatusCode != c {
+		mtr.T.Errorf("Response StatusCode is %d asserted that it is %d", mtr.StatusCode, c)
+		return false
+	}
+	return true
 }
 
 // AssertHeaders ... Helper method to validate the proper headers retuned
-func (mtr *MockTestResponse) AssertHeaders(t *testing.T, expectedHeaders map[string]string) {
-  if len(expectedHeaders) != len(mtr.Head) {
-    mtr.T.Fatalf("expected %v headers; got %v. Returned headers: %v", len(expectedHeaders), len(mtr.Head), mtr.Head)
-  }
-  for key := range expectedHeaders {
-    if val := mtr.Head.Get(key); val != expectedHeaders[key] {
-      mtr.T.Fatalf("expected header %v to be '%v'; got '%v'", key, expectedHeaders[key], val)
-    }
-  }
+func (mtr *MockTestResponse) AssertHeaders(expectedHeaders map[string]string) {
+	if len(expectedHeaders) != len(mtr.Head) {
+		mtr.T.Fatalf("expected %v headers; got %v. Returned headers: %v", len(expectedHeaders), len(mtr.Head), mtr.Head)
+	}
+	for key := range expectedHeaders {
+		if val := mtr.Head.Get(key); val != expectedHeaders[key] {
+			mtr.T.Fatalf("expected header %v to be '%v'; got '%v'", key, expectedHeaders[key], val)
+		}
+	}
+}
+
+func (mtr *MockTestResponse) AssertJson(inst, data interface{}) bool {
+	err := json.Unmarshal(mtr.Bytes(), inst)
+	if err != nil {
+		mtr.T.Errorf("Failed to unmarshal with error: %s", err)
+	}
+	if !reflect.DeepEqual(inst, data) {
+		// Try to extract json text for displaying error
+		txt, err := json.Marshal(data)
+		if err != nil {
+			mtr.T.Errorf("Type %s did not match response %v: expected %v", reflect.TypeOf(data).Kind().String(), mtr.String(), data)
+			return false
+		}
+		mtr.T.Errorf("Type %s did not match response %s: expected %s", reflect.TypeOf(data).Kind().String(), mtr.String(), txt)
+		return false
+	}
+	return true
+
 }
 
 // NewMockTestResponse ... Create an instance of MockTestResponse
 func NewMockTestResponse(t *testing.T) *MockTestResponse {
-  return &MockTestResponse{MockResponse: MockResponse{Buffer: *bytes.NewBuffer(make([]byte, 0)), Head: http.Header{}, StatusCode: -1}, T: t}
+	return &MockTestResponse{MockResponse: MockResponse{Buffer: *bytes.NewBuffer(make([]byte, 0)), Head: http.Header{}, StatusCode: -1}, T: t}
 }
--- http_test.go
+++ http_test.go
@@ -1,61 +1,93 @@
 package mctest
 
 import (
-  "fmt"
-  "net/http"
-  "testing"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
 )
 
 func TestInit(t *testing.T) {
-  req, _ := http.NewRequest("GET", "/path/to/handler", nil)
-  resp := NewMockTestResponse(t)
-  go func(w http.ResponseWriter, r *http.Request) {
+	req, _ := http.NewRequest("GET", "/path/to/handler", nil)
+	resp := NewMockTestResponse(t)
+	go func(w http.ResponseWriter, r *http.Request) {
 
-  }(resp, req)
-  if !resp.AssertCode(-1) {
-    t.Fatalf("Response StatusCode is %d asserted that it is %d", resp.StatusCode, -1)
-  }
-  if len(resp.Bytes()) != 0 {
-    t.Fatalf("Expected response bytes to be 0 but they are %d", len(resp.Bytes()))
-  }
+	}(resp, req)
+	if !resp.AssertCode(-1) {
+		t.Fatalf("Response StatusCode is %d asserted that it is %d", resp.StatusCode, -1)
+	}
+	if len(resp.Bytes()) != 0 {
+		t.Fatalf("Expected response bytes to be 0 but they are %d", len(resp.Bytes()))
+	}
 
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(http.StatusOK)
-  fmt.Fprintf(w, "HomeHandler")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "HomeHandler")
 }
 
 func TestHome(t *testing.T) {
-  req, _ := http.NewRequest("GET", "/path/to/handler", nil)
-  resp := NewMockTestResponse(t)
-  HomeHandler(resp, req)
-  b := "HomeHandler"
-  if !resp.AssertCode(http.StatusOK) {
-    t.Fatalf("Response StatusCode is %d asserted that it is %d", resp.StatusCode, http.StatusOK)
-  }
-  if !resp.AssertBody(b) {
-    t.Fatalf("Response body is %s asserted that it is %s", resp.String(), b)
-  }
+	req, _ := http.NewRequest("GET", "/path/to/handler", nil)
+	resp := NewMockTestResponse(t)
+	HomeHandler(resp, req)
+	b := "HomeHandler"
+	if !resp.AssertCode(http.StatusOK) {
+		t.Fatalf("Response StatusCode is %d asserted that it is %d", resp.StatusCode, http.StatusOK)
+	}
+	if !resp.AssertBody(b) {
+		t.Fatalf("Response body is %s asserted that it is %s", resp.String(), b)
+	}
 }
 
 // TODO: Write some more complex bytes
 func ByteHomeHandler(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(200)
-  w.Write([]byte("HomeHandler"))
+	w.WriteHeader(200)
+	w.Write([]byte("HomeHandler"))
 }
 func TestByteHome(t *testing.T) {
-  req, _ := http.NewRequest("GET", "/path/to/handler", nil)
-  resp := NewMockTestResponse(t)
-  ByteHomeHandler(resp, req)
-  b := "HomeHandler"
-  if string(resp.Bytes()) != "HomeHandler" {
-    t.Fatalf("Expected bytes to equal HomeHandler but failed actual is '%s'", string(resp.Bytes()))
-  }
-  if !resp.AssertCode(200) {
-    t.Fatalf("Response StatusCode is %d asserted that it is %d", resp.StatusCode, 200)
-  }
-  if !resp.AssertBody(b) {
-    t.Fatalf("Response body is %s asserted that it is %s", resp.String(), b)
-  }
+	req, _ := http.NewRequest("GET", "/path/to/handler", nil)
+	resp := NewMockTestResponse(t)
+	ByteHomeHandler(resp, req)
+	b := "HomeHandler"
+	if string(resp.Bytes()) != "HomeHandler" {
+		t.Fatalf("Expected bytes to equal HomeHandler but failed actual is '%s'", string(resp.Bytes()))
+	}
+	if !resp.AssertCode(200) {
+		t.Fatalf("Response StatusCode is %d asserted that it is %d", resp.StatusCode, 200)
+	}
+	if !resp.AssertBody(b) {
+		t.Fatalf("Response body is %s asserted that it is %s", resp.String(), b)
+	}
+}
+
+type Payload struct {
+	X string `json:"x"`
+	Y string `json:"y"`
+}
+
+func testJsonHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	p := &Payload{X: "foo", Y: "bar"}
+	j, err := json.Marshal(p)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Error parsing json %s", err)))
+	}
+	w.Write(j)
+}
+func TestJsonAssert(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/path/to/handler", nil)
+	nt := &testing.T{}
+	resp := NewMockTestResponse(nt)
+	testJsonHandler(resp, req)
+	// Payload mathces so keep moving..
+	if !resp.AssertJson(&Payload{}, &Payload{X: "foo", Y: "bar"}) {
+		t.Fatalf("Failed to validate payload")
+	}
+	// Should fail
+	if resp.AssertJson(&Payload{}, &Payload{X: "boo", Y: "bar"}) {
+		t.Fatalf("Payload does not match!")
+	}
+
 }
